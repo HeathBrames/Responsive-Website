f2dd1a0dd919d0259d8934fac053610b2d5ae38d
Heath Brames <55902552+HeathBrames@users.noreply.github.com>
2020-07-23 13:02:03 -0400

    Adjusted news grid gap & +carousel touch function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
+//Toggle navbar to fade to blue when scrolling
 $(window).scroll(function(){
     $('nav').toggleClass('scrolled', $(this).scrollTop() > 100);
+});
+
+
+//Touch swipe functionality for mobile
+$(".carousel").on("touchstart", function(event){
+    var xClick = event.originalEvent.touches[0].pageX;
+$(this).one("touchmove", function(event){
+    var xMove = event.originalEvent.touches[0].pageX;
+    if( Math.floor(xClick - xMove) > 5 ){
+        $(this).carousel('next');
+    }
+    else if( Math.floor(xClick - xMove) < -5 ){
+        $(this).carousel('prev');
+    }
+});
+$(".carousel").on("touchend", function(){
+        $(this).off("touchmove");
+});
 });
